77afb3307ef7759836f173dd399e763fc459bd42
nvduran <nvduran@gmail.com>
2024-06-29 01:15:50 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+import "./style.css";
+import Phaser, { Physics } from "phaser";
+
+const sizes = {
+        width: 500,
+        height: 500,
+};
+
+const speedDown = 300;
+
+class GameScene extends Phaser.Scene {
+        constructor() {
+                super();
+        }
+
+        preload() {
+                this.load.image("bg", "assets/bg.png");
+        }
+
+        create() {
+                this.add.image(0, 0, "bg").setOrigin(0, 0);
+        }
+
+        update() {}
+}
+
+const config = {
+        type: Phaser.WEBGL,
+        width: sizes.width,
+        height: sizes.height,
+        canvas: document.getElementById("gameCanvas"),
+        physics: {
+                default: "arcade",
+                arcade: {
+                        gravity: { y: speedDown },
+                        debug: false,
+                },
+        },
+        scene: [GameScene],
+};
+
+const game = new Phaser.Game(config);
